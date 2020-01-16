49517b9806e515ede40b01c442a55e45ad6bb828
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-01-15 22:46:22 -0500

    created dashboard, routes are working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './App.css'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import Login from './components/Login'
+import { Route, Link } from 'react-router-dom'
 
 function App () {
   return (
     <div className='App'>
-      <h1>Express Auth frontend</h1>
+      <nav>
+        <div>
+          <Link to='/'>Home</Link>
+        </div>
+        <div>
+          <Link to='/dashboard'>Dashboard</Link>
+        </div>
+        <div>
+          <Link to='/login'>Login</Link>
+        </div>
+      </nav>
+
+      <main>
+        <Route exact path='/' component={Home} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/login' component={Login} />
+      </main>
     </div>
   )
 }
