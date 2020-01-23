7570ee6637a2db8311c0013fa7708706cc9d177e
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-01-22 19:11:18 -0500

    now you can delete your account

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const appRouter = express.Router()
 const { passport } = require('../auth/auth')
-const { Tweet, User } =require('../models')
+const { Tweet, User, Profile } =require('../models')
 
 appRouter.get('/profile', passport.authenticate('jwt', { session: false}),
   async (req, res) => {
     res.json({ user: req.user, message: 'authenticated'})
   }
 )
+//Delete account
+appRouter.delete('/profile/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const deleteUser = await User.findByPk(id)
+    await deleteUser.destroy(req.body)
+    res.json('Your account no longer exists')
+  } catch(error) {
+    console.log(error)
+  }
+})
 //Get all tweets
 appRouter.get('/tweets', async (req, res) => {
   try {
     const allTweets = await Tweet.findAll()
-    console.log(allTweets)
     res.json(allTweets)
   } catch(error) {
     console.log(error)
@@ -55,6 +65,46 @@ appRouter.delete('/tweets/:id', async (req, res) => {
     const deleteTweet = await Tweet.findByPk(id)
     await deleteTweet.destroy(req.body)
     res.json(deleteTweet)
+  } catch(error) {
+    console.log(error)
+  }
+})
+//Get profiles
+appRouter.get('/profile/bio', async (req, res) => {
+  try {
+    const profile = await Profile.findAll()
+    res.json(profile)
+  } catch(error) {
+    console.log(error)
+  }
+})
+//Create a profile
+appRouter.post('/profile/bio', async (req, res) => {
+  try {
+    const newProfile = await Profile.create(req.body)
+    res.json(newProfile)
+  } catch(error){
+    console.log(error)
+  }
+})
+//Edit a profile
+appRouter.put('/profile/bio/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const editProfile = await Profile.findByPk(id)
+    await editProfile.update(req.body)
+    res.json(editProfile)
+  } catch (error) {
+    console.log(error)
+  }
+})
+//Delete your profile
+appRouter.delete('/profile/bio/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const deleteProfile = await Profile.findByPk(id)
+    await deleteProfile.destroy(req.body)
+    res.json('your profile no longer exists')
   } catch(error) {
     console.log(error)
   }
