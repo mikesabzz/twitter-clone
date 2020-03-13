53c57c5d796033f5ae0030450bcdbc6f8d266fb9
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-03-13 17:04:57 -0400

    added edit tweet

--- client/src/components/CreateComponents/UpdateTweets.js
+++ client/src/components/CreateComponents/UpdateTweets.js
@@ -1,10 +1,46 @@
 import React from 'react'
-import editTweet from '../../services/apiService'
+import {editTweet} from '../../services/apiService'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
-const UpdateTweets = (props) => {
-    return (
-        <div>Update</div>
-    )
+class UpdateTweets extends React.Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            tweetId: this.props.match.params.id,
+            updated: false
+        }
+    }
+    handleUpdate = (e) => {
+        const element = e.target
+        const {name, value} = element
+        const newState = {}
+        newState[name] = value
+        this.setState(newState)
+    }
+    handleSubmit = async (e) => {
+        e.preventDefault()
+        const { userId, name, tweet } = this.state
+        const tweets = { userId, name, tweet}
+        const id = this.state.tweetId
+        await editTweet(id, tweets)
+        this.setState({updated: true})
+    }
+    render(){
+        if (this.state.updated){
+            return <Redirect to='/dashboard/tweets' />
+        }
+        return (
+            <div>Update
+                <form onChange={this.handleUpdate} onSubmit={this.handleSubmit}>
+                    <label htmlFor="tweet">Whats happening?</label>
+                    <br />
+                    <textarea name="tweet"></textarea>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default UpdateTweets
