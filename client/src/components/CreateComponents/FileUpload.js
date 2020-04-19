cb90133b7a5f2b538600ccd729d6ed666f941c17
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-04-18 23:49:29 -0400

    started working on fileUpload in react, changes are in continium.

--- client/src/components/CreateComponents/FileUpload.js
+++ client/src/components/CreateComponents/FileUpload.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import ImageUploader from 'react-images-upload'
 import { uploadImage } from '../../services/apiService'
 
 class FileUpload extends React.Component {
     constructor(props){
         super(props)
         this.state = {
+            pictures: []
         }
     }
-
     render () {
         return (
             <div>
-                File Uploader
+                <form>
+                    <label htmlFor="file">File:</label>
+                  
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
