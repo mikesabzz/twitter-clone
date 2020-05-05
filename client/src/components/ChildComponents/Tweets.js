0253deaa3c171a3e464042cd1f89fba1acb5d248
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-05-04 22:23:15 -0400

    added styling to tweets

--- client/src/components/ChildComponents/Tweets.js
+++ client/src/components/ChildComponents/Tweets.js
@@ -1,8 +1,14 @@
 import React from 'react'
-import { getAllTweets, getAllProfiles, getUserNames, deleteTweet } from '../../services/apiService'
+import { 
+    getAllTweets, 
+    getUserNames, 
+    deleteTweet, 
+    getImages 
+} from '../../services/apiService'
 import { Link } from 'react-router-dom'
 import dateFormat from 'dateformat'
 import CreateTweets from '../CreateComponents/CreateTweets'
+import './styles.css'
 
 class Tweets extends React.Component {
     constructor(props) {
@@ -18,7 +24,7 @@ class Tweets extends React.Component {
     async componentDidMount() {
         await this.getTweets()
         await this.getUser()
-        await this.getProfile()
+        await this.getImage()
     }
     getTweets = async () => {
         try {
@@ -32,8 +38,8 @@ class Tweets extends React.Component {
         const names = await getUserNames()
         this.setState({ names })
     }
-    getProfile = async () => {
-        const photo = await getAllProfiles()
+    getImage = async () => {
+        const photo = await getImages()
         this.setState({ photo })
     }
     handleDelete = async (id) => {
@@ -45,31 +51,39 @@ class Tweets extends React.Component {
         if (this.state.deleted) {
             return window.location.reload()
         }
-        const { tweets } = this.state
-        const { names } = this.state
+        const { tweets, names, photo } = this.state
         const sortTweets = tweets.sort((a, b) => {
             return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
         });
 
         return sortTweets.map(tweet => {
             return names.map(name => {
-                if (name.id == tweet.userId) {
-                    return <div className="border border-dark" key={tweet.id}>
-                        <Link to={{ pathname: `/dashboard/user/${name.name}/${name.id}`, state: { names: name } }} className="text-dark">{name.name}</Link>
-                        <p className="text-secondary font-weight-normal">{dateFormat(tweet.createdAt, "mmm dd, yyyy")}</p>
+                return photo.map(img => {                
+                if ((name.id == tweet.userId) && (name.id == img.userId)) {
+                    return <div className="border border-dark w-75" key={tweet.id}>
+                        <img className="tweet-image" src={window.location.origin + `/uploads/${img.poster}`} />
+                        <div id="tweet-container">
+                        <Link to={{ pathname: `/dashboard/user/${name.name}/${name.id}`, state: { names: name } }} 
+                            className="text-dark h4 font-weight-bold">
+                                {name.name}
+                        </Link>
+                        <p className="tweet-date text-secondary font-weight-normal">
+                                {dateFormat(tweet.createdAt, "mmm dd, yyyy")}</p>
                         <div className="font-weight-normal">{tweet.tweet}</div>
+                        </div>
                         {localStorage.getItem('userId') == tweet.userId ?
-                            <div>
-                                <button><Link to={{ pathname: `/dashboard/tweet/${tweet.id}/update`, state: { editTweet: tweet.tweet } }}>Edit</Link></button>
-                                <button onClick={() => this.handleDelete(tweet.id)}>Delete</button></div> : ""
+                            <div className="tweet-edit-delete-button">
+                                <button><Link to={{ pathname: `/dashboard/tweet/${tweet.id}/update`, state: { editTweet: tweet.tweet } }}>
+                                    <span className="glyphicon glyphicon-edit"></span></Link>
+                                </button>
+                                <button onClick={() => this.handleDelete(tweet.id)}><span className="glyphicon glyphicon-trash"></span></button></div> : ""
                         }
                     </div>
                 }
             })
+            })
         })
     }
-
-
 
     render() {
         return (
