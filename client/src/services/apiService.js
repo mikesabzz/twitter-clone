c65585bf009211890cb190ac1a0551e5a3a1c2d6
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-05-06 13:51:29 -0400

    typo

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:4567' 
-// || process.env.REACT_APP_HEROKU_URL 
+const BASE_URL = 'http://localhost:4567' || process.env.REACT_APP_HEROKU_URL 
 
 const JWT_TOKEN = localStorage.getItem('token')
 
--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const profileModel = require('./profile')
 const bcrypt = require('bcrypt')
 const imageModel = require('./image')
 
-const db = new Sequelize({
+const db = new Sequelize((process.env.DATABASE_URL || 'postgres://localhost:5432/twitter_clone_db'),{
   database: 'twitter_clone_db',
   dialect: 'postgres'
 })
