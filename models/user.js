7d32901c71e418113961fdeadae2c667bfc50512
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-05-13 22:49:16 -0400

    JSON.Stringify didnt solve it

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 module.exports = (db, Sequelize) => {
     return db.define('user', {
       name: Sequelize.STRING,
-      email: JSON.stringify({
+      email: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
         validate: {
           isEmail: true
         }
-      }),
-      password: JSON.stringify({
+      },
+      password: {
         type: Sequelize.STRING,
         allowNull: false
-      })
+      }
     })
   }
