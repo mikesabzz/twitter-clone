1e07db732ecf0cd364e0408c5b9d4cc4479395eb
Michael Sabzevari <michaelsabzevari@gmail.com>
2020-05-17 12:31:42 -0400

    i

--- models/index.js
+++ models/index.js
@@ -5,26 +5,15 @@ const profileModel = require('./profile')
 const bcrypt = require('bcrypt')
 const imageModel = require('./image')
 
-const db = new Sequelize((process.env.DATABASE_URL || 'postgres://localhost:5432/twitter_clone_db'),{
-  database: 'twitter_clone_db',
-  dialect: 'postgres',
-  define: {
-    underscored: true,
-    returning: true
-  }
-})
 if (process.env.NODE_ENV === 'production') {
-  // If the node environment is production, connect to a remote PSQL database
-  const db = new Sequelize(process.env.DATABASE_URL , {
-    dialect: 'postgres'
-  });
-}
-else {
-  // Else connect to a local instance of PSQL running on your machine
+const db = new Sequelize(process.env.DATABASE_URL , {
+  dialect: 'postgres'
+})
+} else {
   const db = new Sequelize({
-    database: '', // Name of your local database
+    database: 'twitter_clone_db',
     dialect: 'postgres'
-  });
+  })
 }
 
 const User = UserModel(db, Sequelize)
