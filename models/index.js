4fadc8ab1d71d8f4077f5b3c7975da1ee200e66a
mikesabzz <michaelsabzevari@gmail.com>
2024-01-16 17:28:54 -0500

    bcrypt

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const UserModel = require('./user')
 const TweetModel = require('./tweet')
 const profileModel = require('./profile')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const imageModel = require('./image')
 
 const db = new Sequelize((process.env.DATABASE_URL || 'postgres://localhost:5432/twitter_clone_db'),{
