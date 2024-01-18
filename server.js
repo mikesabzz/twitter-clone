7327229abb92d5223a72641546beeeaaec796246
mikesabzz <michaelsabzevari@gmail.com>
2024-01-18 16:38:58 -0500

    build

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500)
   res.json({ message: err.message })
 })
+
 app.use(express.static(path.join(__dirname, './client/build')))
 
 if (process.env.NODE_ENV == "production") {
